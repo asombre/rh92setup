99e5837541bd2e258fd09e391c3e64620151156f
asombre <34612529+asombre@users.noreply.github.com>
2024-10-29 20:50:23 -0600

    Updated 9_vmsetup.sh
    Added latest virt-install

--- 9_vmsetup.sh
+++ 9_vmsetup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 ## osinfo-query os name="Red Hat Enterprise Linux 9.2"
+#vmname=rhel94-$(date +%s)
+#punum=4
+#emnum=4096
+#imgpath=/var/lib/libvirt/images
+#isopath=$imgpath/rhel-9.4-x86_64-dvd.iso
+#diskpath=$imgpath/$vmname.qcow2,size=60
+#ksfile=textstd.ks
+#kspath=/home/$USER/Documents/kickstarts/$ksfile
+
+#sudo virt-install \
+#--name $vmname \
+#--vcpus $cpunum \
+#--memory $memnum \
+#--location $isopath \
+#--disk $diskpath \
+#--initrd-inject $kspath \
+#--extra-args inst.ks="file:/$ksfile console=tty0 console=ttyS0,115200" \
+#--osinfo rhel9.4 \
+#--nographics
 
 servername=server-1
 
